8e358500ae7ed8ee2127467d969fa0ec797f7ec5
todokr <shunsuke.tadokoro@bizreach.co.jp>
2018-10-13 22:17:44 +0900

    20

--- src/nlp100/section3.clj
+++ src/nlp100/section3.clj
@@ -1,12 +1,14 @@
 (ns nlp100.section3
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io]
+            [clojure.data.json :as json]))
 
 ;; 20
-(read-gzip )
-
 (defn abount-brit [file-name]
   (with-open [f (io/reader (java.util.zip.GZIPInputStream.
                             (io/input-stream file-name)))]
-    (prn (first (line-seq f)))))
+    (->> (line-seq f)
+         (map json/read-str)
+         (filter (fn [{title "title"}] (re-matches #"イギリス" title)))
+         (doall))))
 
 (abount-brit "./resources/jawiki-country.json.gz")
