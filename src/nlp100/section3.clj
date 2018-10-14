b82cb82b655a8b69f8c19bbe01872c89d7b3d177
todokr <shunsuke.tadokoro@bizreach.co.jp>
2018-10-14 15:40:29 +0900

    21

--- src/nlp100/section3.clj
+++ src/nlp100/section3.clj
@@ -1,14 +1,23 @@
 (ns nlp100.section3
   (:require [clojure.java.io :as io]
-            [clojure.data.json :as json]))
+            [clojure.data.json :as json]
+            [clojure.string :as string]))
 
 ;; 20
-(defn abount-brit [file-name]
+(defn extract-uk [file-name]
   (with-open [f (io/reader (java.util.zip.GZIPInputStream.
                             (io/input-stream file-name)))]
     (->> (line-seq f)
          (map json/read-str)
          (filter (fn [{title "title"}] (re-matches #"イギリス" title)))
-         (doall))))
+         (map #(get % "text"))
+         (doall)
+         (first))))
 
-(abount-brit "./resources/jawiki-country.json.gz")
+(spit "uk.txt" (extract-uk "./resources/jawiki-country.json.gz"))
+
+;; 21
+(defn extract-category [text]
+  (string/join "\n" (re-seq #"(?m)^\[\[Category:.*?\]\]$" text)))
+
+(extract-category (extract-uk "./resources/jawiki-country.json.gz"))
